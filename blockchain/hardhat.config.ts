9500fcea76d658c346de426108d2eb223dbf84a9
Ishwak Sharda <ishwak.sharda@gmail.com>
2025-01-19 06:55:44 -0800

    feat: sepolia

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+	solidity: "0.8.28",
+	networks: {
+		sepolia: {
+			accounts: [""],
+			url: "https://eth-sepolia.g.alchemy.com/v2/f6VE6onWndblaVinos8SYCYxq5vvfQkK",
+		},
+	},
 };
 
 export default config;
--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -7,12 +7,17 @@ async function deploy() {
 	const network = await deployer.provider.getNetwork();
 	const chainId = network.toJSON()?.chainId;
 
+	const balance = await deployer.provider.getBalance(deployer.address);
+	console.log(`Deployer balance: ${ethers.formatEther(balance)} ETH`);
+	console.log(`Deploying contracts with account: ${deployer.address}`);
 	const BetFactory = await ethers.getContractFactory("BetFactory");
 	const betFactory = await BetFactory.deploy(deployer.address);
 	await betFactory.waitForDeployment();
 
 	console.log(betFactory.deploymentTransaction());
 	console.log(`BetFactory deployed to: ${await betFactory.getAddress()}`);
+
+	//sepolia: 0x444215c3258cE8c0069E48127F21DA4808C67F65
 }
 deploy()
 	.then(() => process.exit(0))
