8c6aaad76eebe1ec40a054e34cff3681719e9d69
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2025-01-19 10:39:48 -0800

    error fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet"></link>
       </head>
       <body
